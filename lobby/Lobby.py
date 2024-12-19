248b6e44b6dc9db9f7ad16c9c3b4a81640486a81
Frigo23432 <hgarotta@icloud.com>
2024-12-19 08:29:58 +0100

    Fichier type pour le lobby

---
+++ lobby/Lobby.py
@@ -0,0 +1,20 @@
+import pygame
+pygame.init()
+
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Habibi")
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+    pygame.display.flip()
+
+
+    clock.tick(60)
