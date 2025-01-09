452f79c6225d64070be75e18276fda7e6bb0a5fc
Frigo23432 <hgarotta@icloud.com>
2025-01-09 09:52:53 +0100

    Update Lobby.py

--- lobby/Lobby.py
+++ lobby/Lobby.py
@@ -1,38 +1,62 @@
 import pygame
 pygame.init()
-
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Habibi")
 clock = pygame.time.Clock()
-sprite_perso = pygame.image.load("sprites/sprite perso 1.png")
-def add_character_at_location(x,y):
-        screen.blit(sprite_perso, (x,y))
+background = pygame.transform.scale(pygame.image.load("assets/sol.png"),(screen_width,screen_height))
 x = screen_width*0.5
 y = screen_height*0.5
-add_character_at_location(x,y)
-character_move_speed = 10
-y_change = 0
+Player_speed = 8
+running = True
 
-while True:
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("sprites/sprite perso 1.png")
+        self.pos = pygame.math.Vector2(x,y)
+        self.speed = Player_speed
+
+    def user_input(self):
+        self.velocity_x = 0
+        self.velocity_y = 0
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_z]:
+            self.velocity_y = -self.speed
+        if keys[pygame.K_q]:
+            self.velocity_x = -self.speed
+        if keys[pygame.K_s]:
+            self.velocity_y = self.speed
+        if keys[pygame.K_d]:
+            self.velocity_x = self.speed
+    
+    def move(self):
+        self.pos += pygame.math.Vector2(self.velocity_x, self.velocity_y)
+
+    def update(self):
+        self.user_input()
+        self.move()
+
+
+player = Player()
+while running:
+    keys = pygame.key.get_pressed
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                y_change = -character_move_speed
-            elif event.key == pygame.K_DOWN:
-                y_change = character_move_speed
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                y = 0
-    y+= y_change
+            running = False
 
-    
-    pygame.display.update()
+    screen.blit(background,(0,0))
+    screen.blit(player.image, player.pos)
+    player.update()
+        
     pygame.display.flip()
+    
 
 
     clock.tick(60)
+pygame.quit()
+sys.exit()
