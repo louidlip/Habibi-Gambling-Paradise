0ce857707edd2af0426bb551623622089942904a
louidlip <louislipuma@gmail.com>
2025-01-16 08:44:13 +0100

    Update jeu_de_dé.py

--- codes/jeu_de_dé.py
+++ codes/jeu_de_dé.py
@@ -6,7 +6,7 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Habibi")
 clock = pygame.time.Clock()
-Logo = pygame.image.load("assets/logo/Logo.png")
+Logo = pygame.image.load("assets/Logo.png")
 pygame.display.set_icon(Logo)
 font = pygame.font.Font("font/Daydream.ttf",19)
 roll_message = font.render("Appuyer sur ESPACE pour faire tourner le de", True, (255,255,255))
