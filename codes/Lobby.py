a54c5f84a9b7d190b3ab88ddb40e569c49843714
Frigo23432 <hgarotta@icloud.com>
2025-01-16 09:17:06 +0100

    Update Lobby.py

--- codes/Lobby.py
+++ codes/Lobby.py
@@ -1,4 +1,5 @@
-simport pygame
+import pygame
+import sys
 pygame.init()
 screen_width = 800
 screen_height = 600
@@ -8,13 +9,14 @@ clock = pygame.time.Clock()
 background = pygame.transform.scale(pygame.image.load("assets/sol.png"),(screen_width,screen_height))
 x = screen_width*0.5
 y = screen_height*0.5
-Player_speed = 8
+Player_speed = 5
 running = True
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load("assets/sprite perso 1.png")
+        self.rect = self.image.get_rect(center=(x, y))
         self.pos = pygame.math.Vector2(x,y)
         self.speed = Player_speed
 
@@ -26,15 +28,35 @@ class Player(pygame.sprite.Sprite):
 
         if keys[pygame.K_z]:
             self.velocity_y = -self.speed
+            if keys[pygame.K_q] or keys[pygame.K_d]:
+                self.velocity_x=0
+                self.velocity_y=0
         if keys[pygame.K_q]:
             self.velocity_x = -self.speed
+            if keys[pygame.K_z] or keys[pygame.K_s]:
+                self.velocity_x=0
+                self.velocity_y=0
         if keys[pygame.K_s]:
             self.velocity_y = self.speed
+            if keys[pygame.K_q] or keys[pygame.K_d]:
+                self.velocity_x=0
+                self.velocity_y=0
         if keys[pygame.K_d]:
             self.velocity_x = self.speed
-    
+            if keys[pygame.K_z] or keys[pygame.K_s]:
+                self.velocity_x=0
+                self.velocity_y=0
     def move(self):
         self.pos += pygame.math.Vector2(self.velocity_x, self.velocity_y)
+        if self.pos.x < 0:
+            self.pos.x = 0 
+        if self.pos.x > screen_width - self.rect.width:
+            self.pos.x = screen_width - self.rect.width  
+        if self.pos.y < 0:
+            self.pos.y = 0  
+        if self.pos.y > screen_height - self.rect.height:
+            self.pos.y = screen_height - self.rect.height
+        
 
     def update(self):
         self.user_input()
